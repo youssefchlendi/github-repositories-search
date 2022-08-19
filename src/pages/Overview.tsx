4a6a1ef95a4aa4f73bc693154ef67207f13cece4
Mohamed youssef Chlendi <youssef.chlendi@gmail.com>
2022-08-19 12:14:38 +0100

    markdown using current user

--- src/pages/Overview.tsx
+++ src/pages/Overview.tsx
@@ -1,17 +1,25 @@
 import React, { useEffect } from 'react';
 import { marked } from 'marked';
+import { useParams } from 'react-router-dom';
 function Overview() {
 	const [markdown, setMarkdown] = React.useState('');
+	const {userName}  = useParams<{
+		userName: string;
+	}>();
 	useEffect(() => {
-		const markdown = ('https://raw.githubusercontent.com/youssefchlendi/youssefchlendi/main/README.md');
+		const markdown = (`https://raw.githubusercontent.com/${userName}/${userName}/main/README.md`);
 		fetch(markdown)
 			.then(response => {
 				return response.text()
 			})
 			.then(text => {
+				console.log(text);
+				if(!text.includes("404"))
 				setMarkdown(marked(text));
+				else
+				setMarkdown("<div><h1>User has no README</h1></div>");
 			}
-			);
+			)
 	}, []);
 
 	return (
