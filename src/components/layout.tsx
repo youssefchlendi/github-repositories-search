71224305903203b69d3560820993f516cfd7b285
Mohamed youssef Chlendi <youssef.chlendi@gmail.com>
2022-08-21 01:11:20 +0100

    make topMenu stick when in view

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,63 +1,84 @@
 import { useEffect, useState } from 'react';
 import { Outlet, useParams } from 'react-router-dom';
 import SidePanel from '../components/sidepanel/sidepanel';
-import TopPanel from '../components/toppanel';
 import { useAppDispatch, useAppSelector } from '../app/hooks';
 import { fetchDataAsync } from '../store/dataSlice';
 import LoadingSpinner from './loading';
 
 function Layout() {
-	useEffect(() => {   
-		window.addEventListener("scroll", listenToScroll);
-		return () => 
-		   window.removeEventListener("scroll", listenToScroll); 
-	  }, [])
-	const listenToScroll = () => {
-	const winScroll = document.body.scrollTop || 
-		document.documentElement.scrollTop;
-	const top = document.getElementById("topLeftPart");
-	const bottom = document.getElementById("bottomImage");
-	if(top&&bottom){
-		if (!(winScroll > bottom.offsetHeight) ) { 
-			top.style.opacity = "0";
-			bottom.style.opacity = "1";
-		} else {
-			top.style.opacity = "1";
-			bottom.style.opacity = "0";
-		}  
-	}
-	};
+	let initialDistance = 0;
+	const loading = useAppSelector(state => state.data.loading)
 	
-	const {userName}  = useParams<{
+	useEffect(() => {
+		window.addEventListener("scroll", listenToScroll);
+		return () =>
+			window.removeEventListener("scroll", listenToScroll);
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [loading])
+	const listenToScroll = () => {
+		const winScroll = document.body.scrollTop ||
+			document.documentElement.scrollTop;
+		const top = document.getElementById("topLeftPart");
+		const bottom = document.getElementById("bottomImage");
+		const topContainer = document.getElementsByClassName("fixedMenu")[0] as HTMLElement;
+		const navMenu = document.getElementsByClassName("navMenu")[0] as HTMLElement;
+		const homeButton = document.getElementsByClassName("homeButton")[0] as HTMLElement;
+		if (top && bottom && topContainer) {
+			if (!(winScroll > bottom.offsetHeight)) {
+				top.style.opacity = "0";
+				bottom.style.opacity = "1";
+			} else {
+				top.style.opacity = "1";
+				bottom.style.opacity = "0";
+			}
+			if(!initialDistance) {
+				initialDistance = topContainer.offsetTop-topContainer.offsetHeight;
+			}
+			if (initialDistance-8 < winScroll) {
+				console.log("scrolled");
+				homeButton.style.display = "block";
+				topContainer.style.position = "fixed";
+				topContainer.style.top = "0";
+				topContainer.style.marginTop = "0";
+				navMenu.style.marginTop="0";
+			} else { 
+				homeButton.style.display = "none";
+				topContainer.style.position = "relative";
+				topContainer.style.top = "0";
+				topContainer.style.marginTop = "8px";
+				navMenu.style.marginTop="8px";
+			}
+		}
+	};
+
+	const { userName } = useParams<{
 		userName: string;
 	}>();
 	const dispatch = useAppDispatch();
 	const data = useAppSelector(state => state.data.data);
-	const loading = useAppSelector(state=>state.data.loading)
 	const [dataloaded, setDataloaded] = useState(false);
 	useEffect(() => {
-		if(!dataloaded){
-			if(userName){
-				dispatch(fetchDataAsync({search:userName}));
+		if (!dataloaded) {
+			if (userName) {
+				dispatch(fetchDataAsync({ search: userName }));
 				setDataloaded(true);
 			}
 		}
-	} , [dataloaded, dispatch, loading, userName]);
+	}, [dataloaded, dispatch, loading, userName]);
 
 	return (
 		<div className="">
-			{loading ? <LoadingSpinner/>:<></>}
-			<TopPanel />
-		<div className="App">
-			<div className="container">
+			{loading ? <LoadingSpinner /> : <></>}
+			<div className="App">
+				<div className="container">
 
-				<SidePanel
-					bio={data.bio}
-					organization={data.organizations}
-				/>
-				<Outlet></Outlet>
+					<SidePanel
+						bio={data.bio}
+						organization={data.organizations}
+					/>
+					<Outlet></Outlet>
+				</div>
 			</div>
-		</div>
 		</div>
 	);
 }
